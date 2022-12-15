73334c8aab9845683b263cf77ecbedc314bdf1db
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-15 11:48:18 +0100

    implementation of lambda expression

--- LinqTutorial15dec2022_2/Program.cs
+++ LinqTutorial15dec2022_2/Program.cs
@@ -5,25 +5,17 @@ internal class Program
     static void Main(string[] args)
     {
         var numbers = new[] { 1, 2, 3, 99, 256, 2 };
-        bool isAnyLargerThan100 = IsAny(numbers, IsLargerThan100); // passing a function as a prameter
-        bool isAnyEven = IsAny(numbers, IsEven);
+        bool isAnyLargerThan100 = IsAny(numbers, number => number > 100); // passing a function as a prameter
+        bool isAnyEven = IsAny(numbers, number => number % 2 == 0);
 
         Console.WriteLine($"Is there any number > 100?: {isAnyLargerThan100}");
         Console.WriteLine($"Is there any number that is even?: {isAnyEven}");
     }
 
 
-    private static bool IsLargerThan100(int number)
-    {
-        return number > 100;
-    }
-    private static bool IsEven(int number)
-    {
-        return number % 2 == 0;
-    }
 
     //general pattern of a method:
-    private static bool IsAny(int[] numbers, Func<int, bool> predicate
+    private static bool IsAny(int[] numbers, Func<int, bool> predicate /*the method declared with the lambda expression is used in place of this aprameter*/
         /*a function is taking a number and returning a bool*/)
         // Func type that represents a function, first generic is the type of input parameter,
         // second is the type of return value
